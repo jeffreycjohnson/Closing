9b4b18fb70c41a70707f2ad50ca1e36b40cb29c2
patrickgh3 <patrickjtraynor@gmail.com>
2015-01-23 21:21:39 -0800

    Door script

--- Assets/Scripts/DoorClose.cs
+++ Assets/Scripts/DoorClose.cs
@@ -5,17 +5,29 @@ public class DoorClose : MonoBehaviour
 {
 
     public float CloseTime = 10f; // time to close in seconds
+    public float closeDist = 1f; // How far the door moves (
 
-    private Vector3 openPos;
-    private float closeDist = 1f;
+    private float endY;
+    private bool doneMoving = false;
 
-	void Start ()
+	void Start()
     {
-	    
+        endY = transform.position.y;
+        transform.transform.transform.Translate(0, closeDist, 0);
 	}
 	
-	void Update ()
+	void Update()
     {
-        transform.Translate(0, -closeDist * Time.deltaTime / CloseTime, 0);
+        if (!doneMoving)
+        {
+            transform.Translate(0, -closeDist * Time.deltaTime / CloseTime, 0);
+            if (transform.position.y < endY)
+            {
+                doneMoving = true;
+                transform.position = new Vector3(transform.position.x, endY, transform.position.z);
+                Debug.Log("Door closed");
+                // hook for restart level and stuff here
+            }
+        }
 	}
 }
