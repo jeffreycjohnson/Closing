525a9eda8d01466cf8785e33747d5d5a65d569fa
Jeffrey Johnson <jeffcjohnson@live.com>
2015-01-23 22:58:23 -0800

    Level Loading actually working

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,18 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Net.Mail;
+using UnityEngine;
 using System.Collections;
 
 public class Level : MonoBehaviour
 {
+    public static int LevelCount = 2;
+    private static Vector3 _levelPos = new Vector3(0, 0, 0);
     private static int _levelIndex = 0;
-    public static int LevelCount = 1;
+    public int OffsetX, OffsetY, OffsetZ;
     void Start()
     {
-        Next();
+        transform.position = _levelPos;
+        _levelPos += new Vector3(OffsetX, OffsetY, OffsetZ);
+        if (_levelIndex == 0)
+        {
+            Next();
+        }
     }
 
     public static void Next()
     {
-        if (_levelIndex < LevelCount)
+        if (_levelIndex + 1 < LevelCount)
         {
             _levelIndex++;
             Application.LoadLevelAdditive(_levelIndex);
