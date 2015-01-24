3fd64db39e6c47de23808fbf55aa2be4d050861f
patrickgh3 <patrickjtraynor@gmail.com>
2015-01-23 20:59:33 -0800

    Test level, start of door script

---
+++ Assets/Scripts/DoorClose.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorClose : MonoBehaviour
+{
+
+    public float CloseTime = 10f; // time to close in seconds
+
+    private Vector3 openPos;
+    private float closeDist = 1f;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+        transform.Translate(0, -closeDist * Time.deltaTime / CloseTime, 0);
+	}
+}
